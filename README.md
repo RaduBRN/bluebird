e7b7707583df995005cd8bf4b9d4559e1c9fc817
RaduBRN <90260247+RaduBRN@users.noreply.github.com>
2023-04-02 11:13:55 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Bluebird
+
+This is a JavaScript app that displays a tweet feed, inspired by the popular social media platform Twitter. Users can like, retweet, and reply to tweets. The app is built using vanilla JavaScript and uses hardcoded data from a data file.
+
+## Usage
+
+Open the index.html file in a web browser to view the tweet feed.
+
+Users can interact with the tweets by clicking on the like, retweet, or reply buttons. Clicking on the tweet button will add a new tweet to the top of the feed.
+
+## Dependencies
+
+The app uses the uuid library from https://jspm.dev/uuid to generate unique identifiers for new tweets.
---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [
+  {
+    handle: `@DollaDollaBills💵`,
+    profilePic: `images/spammer.jpeg`,
+    likes: 47,
+    retweets: 30,
+    tweetText: `🚨ATTENTION🚨 If you want to make some serious cash, follow me and click the link in my bio! 🔗 You don't want to miss out on this amazing opportunity to make money from home! 💰💸`,
+    replies: [],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "4b161eee-c0f5-4545-9c4b-8562944223ee",
+  },
+  {
+    handle: `@QT ✅`,
+    profilePic: `images/tarantino.jpeg`,
+    likes: 4730,
+    retweets: 684,
+    tweetText: `Roles up for grabs, baby! Bring your A-game and make a name in the biz. Don't miss out, it's gonna be killer 🎥`,
+    replies: [
+      {
+        handle: `@TheRealSLJ ✅`,
+        profilePic: `images/jackson.jpeg`,
+        tweetText: `You know I'm always up for it`,
+      },
+      {
+        handle: `@TomCruise ✅`,
+        profilePic: `images/tcruise.png`,
+        tweetText: `I'm your guy, Quentin 🤩`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "3c23454ee-c0f5-9g9g-9c4b-77835tgs2",
+  },
+  {
+    handle: `@TechNewbie25`,
+    profilePic: `images/portrait.jpeg`,
+    likes: 5,
+    retweets: 1,
+    tweetText: `Is it possible to learn coding without a degree??`,
+    replies: [
+      {
+        handle: `@CodeSucks22`,
+        profilePic: `images/overflow.png`,
+        tweetText: `Why bother? You'll never make it without a degree. Might as well give up now and save yourself the trouble. 😒`,
+      },
+      {
+        handle: `@CodeMaster7 🧑‍💻`,
+        profilePic: `images/love.png`,
+        tweetText: `Absolutely! It's all about your passion and dedication. Keep learning and practicing, and you can go far. 👍`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "8hy671sff-c0f5-4545-9c4b-1237gyys45",
+  },
+];
